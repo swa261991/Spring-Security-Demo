8cc7949d58480c68d39fdc65eb4048b5a8dad04d
hard_coder <swapniljadhav261991@gmail.com>
2021-11-17 05:53:42 +0530

    spaces

--- src/main/java/com/luv2code/springsecurity/demo/config/SecurityWebApplicationInitializer.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -2,7 +2,7 @@ package com.luv2code.springsecurity.demo.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
-public class SecurityWebApplicationInitializer 
+public class SecurityWebApplicationInitializer
 						extends AbstractSecurityWebApplicationInitializer {
 
 }
